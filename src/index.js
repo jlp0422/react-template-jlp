b08c7d683244f35ba4c19f9b8d2b0607fc4bd254
Jeremy Philipson <jeremyphilipson@gmail.com>
2019-02-10 15:51:39 -0500

    test-2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
-/* eslint-enable no-unused-vars */
-
 import ReactDOM from "react-dom";
 
-/* eslint-disable no-undef */
 const app = document.getElementById("app");
-/* eslint-enable no-undef */
-
 const title = "My minimal React Webpack Babel app";
 
 ReactDOM.render(<div>{title}</div>, app);
